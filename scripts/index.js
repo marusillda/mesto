a5ced233c0a33a6a442ffcd72ee45cc10af97450
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2022-12-12 22:18:47 -0500

    fix: исправить замечания после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,46 @@
+//Находим DOM-элементы
 const profileNameElement = document.querySelector('.profile__name');
 const profileAboutElement = document.querySelector('.profile__about');
 const popupNameElement = document.querySelector('#profile_name');
 const popupAboutElement = document.querySelector('#profile_about');
-const popupContainer = document.querySelector('.popup__container');
+const popupForm = document.querySelector('.popup__form');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 
-const profileName = profileNameElement.textContent;
-const profileAbout = profileAboutElement.textContent;
-
-popupNameElement.value = profileName;
-popupAboutElement.value = profileAbout;
-
-function popupSubmitted(event){
+/**
+ * Обработчик события Form Submit
+ * @param {Event} event
+ */
+function handleFormSubmit(event){
   event.preventDefault();
 
-  const newProfileName = popupNameElement.value;
-  const newProfileAbout = popupAboutElement.value;
+  profileNameElement.textContent = popupNameElement.value;
+  profileAboutElement.textContent = popupAboutElement.value;
 
-  profileNameElement.textContent = newProfileName;
-  profileAboutElement.textContent = newProfileAbout;
+  handlePopupClose();
 }
 
-function popupClosed(){
+/**
+ * Обработчик закрытия Popup
+ */
+function handlePopupClose(){
   popup.classList.remove('popup_opened');
 }
 
-function profileEdit(){
+/**
+ * Обработчик открытия Popup с заполением Name и About со страницы
+ */
+function handlePopupOpen(){
+  popupNameElement.value = profileNameElement.textContent;
+  popupAboutElement.value = profileAboutElement.textContent;
+
   popup.classList.add('popup_opened');
 }
 
-popupContainer.addEventListener('submit', popupSubmitted);
-popupCloseButton.addEventListener('click', popupClosed);
-profileEditButton.addEventListener('click', profileEdit);
+//Обработчики событий
+popupForm.addEventListener('submit', handleFormSubmit);
+popupCloseButton.addEventListener('click', handlePopupClose);
+profileEditButton.addEventListener('click', handlePopupOpen);
 
 
